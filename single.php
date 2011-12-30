90ae9d62fa9bc6eb7325d85838fbd5fb2f36a47f
Peter Keating <peter@moov2.com>
2011-12-30 13:38:14 +0000

    added content to the single post page.

--- single.php
+++ single.php
@@ -1,9 +1,19 @@
 <?php get_header(); ?>
 
+	<?php while ( have_posts() ) : the_post() ?>
 	<article>
+		<h1><?php the_title(); ?></h1>
 		
+		<div class="published-on">Posted at <?php the_time('g:i a') ?> on <?php the_time('F j, Y') ?></div>
 		
-	
+		<?php the_content(); ?>
 	</article>
+	
+	<?php endwhile ?>
 
+	<nav>
+		<div class="nav-previous"><?php previous_post_link( '%link', __( 'Previous', 'peterkeating.co.uk' ) ); ?></div>
+		<div class="nav-next"><?php next_post_link( '%link', __( 'Next', 'peterkeating.co.uk' ) ); ?></div>
+	</nav>
+	
 <?php get_footer(); ?>
