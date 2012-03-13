312641c9dac4aa0e923aa3af567d0bf8d57052a9
Peter Keating <peter@moov2.com>
2012-03-13 17:46:45 +0000

    added google analytics code.

--- src/footer.php
+++ src/footer.php
@@ -8,6 +8,13 @@
 	
 	<script src="<?php bloginfo( "template_url" ); ?>/js/plugins.js"></script>
 	<script src="<?php bloginfo( "template_url" ); ?>/js/scripts.js"></script>
+
+	<script>
+		var _gaq=[['_setAccount','UA-22730259-1'],['_trackPageview']];
+		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+		s.parentNode.insertBefore(g,s)}(document,'script'));
+	</script>
 	
 </body>
 </html>
