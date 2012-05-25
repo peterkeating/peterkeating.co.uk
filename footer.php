fea533550745b8f27e16e5ff5da3af95879ce80c
Peter Keating <peter@moov2.com>
2012-05-25 15:58:20 +0100

    now loading JQuery from Google CDN.

--- footer.php
+++ footer.php
@@ -6,8 +6,10 @@
 		</div>
 	</footer>
 	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  	<script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+
 	<!-- scripts concatenated and minified via build script -->
-	<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.7.2.min.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 	<!-- end scripts -->
 
