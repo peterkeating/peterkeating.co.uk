6c3f5bd51eaefcc2e8e3cae3a647cc64bb466bd0
Peter Keating <peter@moov2.com>
2012-08-17 15:55:27 +0100

    google analytics code is not added to the footer if preview=true is in the url.

--- footer.php
+++ footer.php
@@ -13,12 +13,15 @@
 	<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
 	<!-- end scripts -->
 
+	<?php
+	if($_GET['preview'] != "true") {
+	?>
 	<script>
 		var _gaq=[['_setAccount','UA-22730259-1'],['_trackPageview']];
 		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 		s.parentNode.insertBefore(g,s)}(document,'script'));
 	</script>
-	
+	<?php } ?>
 </body>
 </html>
