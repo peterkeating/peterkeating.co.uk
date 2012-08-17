546a86a167a26770580dc6dbc60777d6b6cf42be
Peter Keating <peter@moov2.com>
2012-08-17 16:04:19 +0100

    updated to JQuery 1.8.0.

--- footer.php
+++ footer.php
@@ -6,8 +6,8 @@
 		</div>
 	</footer>
 	
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  	<script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.7.2.min.js"><\/script>')</script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min.js"></script>
+  	<script>window.jQuery || document.write('<script src="<?php bloginfo('template_url'); ?>/js/libs/jquery-1.8.0.min.js"><\/script>')</script>
 
 	<!-- scripts concatenated and minified via build script -->
 	<script src="<?php bloginfo('template_url'); ?>/js/main.js"></script>
