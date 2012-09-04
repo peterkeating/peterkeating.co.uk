b778a6b5ed5fd318eca7371b38459ef374136be2
Peter Keating <peter@moov2.com>
2012-09-04 19:15:26 +0100

    implemented the archive that now shows all the posts on the blog broken into month sections.

--- archive.php
+++ archive.php
@@ -6,16 +6,33 @@ Template Name: Archive
 
 <?php get_header(); ?>
 
-<?php the_post(); ?>
+<?php
+// gets every post.
+query_posts('posts_per_page=-1');
 
-<article class="clearfix">
-	<div class="post-details">
-		<h1 class="post-title"><?php the_title(); ?></h1>
+$active = '';
 
-		<div class="post">
-			<?php the_content(); ?>
-		</div>    
-	</div>
+?>
+
+<div class="archive">
+	<h1 class="title">Archive</h1>
+
+ 	<?php while ( have_posts() ) : the_post(); ?>
+ 	<?php 
+ 		if ($active != substr($post->post_date, 0, 7)) { 
+ 			$active = substr($post->post_date, 0, 7);
+ 		?>
+ 			<div class="month-year"><?php the_time('F Y') ?></div>
+ 		<?php } ?>
+
+ 		<div class="post clearfix">
+ 			<div class="day">
+ 				<?php the_time('j') ?>
+ 			</div> 
+
+ 			<a href="<?php the_permalink(); ?>"><?php the_title() ?></a>
+ 		</div>
+ 	<?php endwhile; ?>
 </div>
 
 <?php get_footer(); ?>
