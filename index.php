516d85270aa882d2be491afbf9fa484f9689785e
Peter Keating <peter@moov2.com>
2012-09-05 09:34:40 +0100

    slight changes to the post summary display.

--- index.php
+++ index.php
@@ -4,13 +4,15 @@
 <?php while ( have_posts () ) : the_post() ?>
 
 	<div class="post-summary">
-		<div class="title"><?php the_title(); ?></div>
+		<div class="title medium-large-text"><?php the_title(); ?></div>
 		<div class="publish-date small-medium-text"><?php the_time('F j, Y') ?></div>
 		<div class="snippet"><?php the_excerpt(); ?></div>
 
-		<a href="<?php the_permalink(); ?>" class="post-link">Read More</a>
+		<a href="<?php the_permalink(); ?>" class="post-link">Continue Reading</a>
 	</div>
 
 <?php endwhile; ?>
+
+<a href="/index.php/archive">Check out the Archive to view all the posts.</a>
 
 <?php get_footer(); ?>
