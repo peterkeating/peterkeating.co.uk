d1be246b95f1157b2b8082d379f0a6271ecafdeb
Peter Keating <peter@moov2.com>
2012-09-07 11:18:57 +0100

    post summary now has a 'wide' view that shows the date, tags and post link on the left hand side of the title & snippet when the screen size is large. If the screen size isn't deemed large enough the the narrow view comes in which is just the original where the date & tags are merged in with the title & snippet.

--- index.php
+++ index.php
@@ -3,17 +3,25 @@
 
 <?php while ( have_posts () ) : the_post() ?>
 
-	<div class="post-summary">
-		<h2 class="title"><?php the_title(); ?></h2>
-		
-		<div class="clearfix">
-			<div class="publish-date small-medium-text date-icon"><?php the_time('F j, Y') ?></div>
+	<div class="post-summary clearfix">
+		<div class="wide-post clearfix">
+			<div class="publish-date date-icon"><?php the_time('F j, Y') ?></div>
 			<div class="tags small-medium-text tag-icon"><?php the_tags('',', ',''); ?></div>
+			<a href="<?php the_permalink(); ?>" class="post-link">Continue Reading</a>
 		</div>
 
-		<div class="snippet"><?php the_excerpt(); ?></div>
+		<div class="post-keyinfo">
+			<h2 class="title"><?php the_title(); ?></h2>
+			
+			<div class="narrow-post clearfix">
+				<div class="publish-date small-medium-text date-icon"><?php the_time('F j, Y') ?></div>
+				<div class="tags small-medium-text tag-icon"><?php the_tags('',', ',''); ?></div>
+			</div>
 
-		<a href="<?php the_permalink(); ?>" class="post-link">Continue Reading</a>
+			<div class="snippet"><?php the_excerpt(); ?></div>
+
+			<a href="<?php the_permalink(); ?>" class="post-link">Continue Reading</a>
+		</div>
 	</div>
 
 <?php endwhile; ?>
