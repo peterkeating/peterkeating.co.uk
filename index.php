6baa3c692f3eb8c55ce5798dbe036bbd5002bda6
Peter Keating <peter@moov2.com>
2013-02-02 22:42:18 +0000

    refactored the HTML & CSS for the post summaries.

--- index.php
+++ index.php
@@ -3,22 +3,22 @@
 
 <?php while ( have_posts () ) : the_post() ?>
 
-	<div class="post-summary clearfix">
-		<div class="wide-post clearfix">
-			<div class="publish-date date-icon"><?php the_time('F j, Y') ?></div>
-			<div class="tags small-medium-text tag-icon"><?php the_tags('',', ',''); ?></div>
+	<div class="post-summary">
+		<div class="wide-post">
+			<div class="post-publish-date date-icon"><?php the_time('F j, Y') ?></div>
+			<div class="post-tags small-medium-text tag-icon"><?php the_tags('',', ',''); ?></div>
 			<a href="<?php the_permalink(); ?>" class="post-link">Continue Reading</a>
 		</div>
 
 		<div class="post-keyinfo">
-			<h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-			
-			<div class="narrow-post clearfix">
-				<div class="publish-date small-medium-text date-icon"><?php the_time('F j, Y') ?></div>
-				<div class="tags small-medium-text tag-icon"><?php the_tags('',', ',''); ?></div>
+			<h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+			<div class="narrow-post">
+				<div class="post-publish-date date-icon"><?php the_time('F j, Y') ?></div>
+				<div class="post-tags tag-icon"><?php the_tags('',', ',''); ?></div>
 			</div>
 
-			<div class="snippet"><?php the_excerpt(); ?></div>
+			<div class="post-snippet"><?php the_excerpt(); ?></div>
 
 			<a href="<?php the_permalink(); ?>" class="post-link">Continue Reading</a>
 		</div>
