0a76a549febb2581d9f91dc3eb6948f4c4f4fce9
Peter Keating <peter@moov2.com>
2013-02-02 23:28:43 +0000

    fixed layout of archive page by removing clearfix from the markup and using @extend in the CSS.

--- archive.php
+++ archive.php
@@ -18,19 +18,19 @@ $active = '';
 	<h1 class="title">Archive</h1>
 
  	<?php while ( have_posts() ) : the_post(); ?>
- 	<?php 
- 		if ($active != substr($post->post_date, 0, 7)) { 
+ 	<?php
+ 		if ($active != substr($post->post_date, 0, 7)) {
  			$active = substr($post->post_date, 0, 7);
  		?>
  			<h2 class="month-year"><?php the_time('F Y') ?></h2>
  		<?php } ?>
 
- 		<div class="post clearfix">
+ 		<div class="post">
  			<div class="day">
  				<small class="element">
  					<?php the_time('j') ?>
  				</small>
- 			</div> 
+ 			</div>
 
  			<div class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></div>
  		</div>
