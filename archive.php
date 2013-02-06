68701d63114a1a00fdb6183d3f268877927cfd5a
Peter Keating <peter@moov2.com>
2013-02-06 21:02:27 +0000

    refactored the CSS & HTML for the archives page to ensure CSS isn't inherited from other areas of the site, use variables and improve layout.

--- archive.php
+++ archive.php
@@ -15,24 +15,24 @@ $active = '';
 ?>
 
 <div class="archive">
-	<h1 class="title">Archive</h1>
+	<h1>Archive</h1>
 
  	<?php while ( have_posts() ) : the_post(); ?>
  	<?php
  		if ($active != substr($post->post_date, 0, 7)) {
  			$active = substr($post->post_date, 0, 7);
  		?>
- 			<h2 class="month-year"><?php the_time('F Y') ?></h2>
+ 			<h2 class="archive-month-year"><?php the_time('F Y') ?></h2>
  		<?php } ?>
 
- 		<div class="post">
- 			<div class="day">
- 				<small class="element">
+ 		<div class="archive-post">
+ 			<div class="archive-post-date">
+ 				<small class="archive-post-date-day">
  					<?php the_time('j') ?>
  				</small>
  			</div>
 
- 			<div class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></div>
+ 			<div class="archive-post-title"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></div>
  		</div>
  	<?php endwhile; ?>
 </div>
