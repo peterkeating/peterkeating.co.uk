298024283c4bcb8b3351cd4c5b8d6c48e25dbb70
Peter Keating <peter@moov2.com>
2013-02-06 20:15:02 +0000

    refactored the header CSS to reduce markup, use variables and improve class names.

--- header.php
+++ header.php
@@ -1,62 +1,60 @@
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
-	<meta charset="utf-8">
+    <meta charset="utf-8">
 
-	<title>Peter Keating</title>
-	<meta name="description" content="">
-	<meta name="author" content="Peter Keating">
+    <title>Peter Keating</title>
+    <meta name="description" content="">
+    <meta name="author" content="Peter Keating">
 
-	<meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
 
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" />
-	<link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/master.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css" />
+    <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/master.css" />
 
-	<script src="<?php bloginfo( "template_url" ); ?>/js/libs/modernizr-2.6.2.min.js"></script>
+    <script src="<?php bloginfo( "template_url" ); ?>/js/libs/modernizr-2.6.2.min.js"></script>
 
-	<script type="text/javascript" src="//use.typekit.net/men0iqb.js"></script>
-	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    <script type="text/javascript" src="//use.typekit.net/men0iqb.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
 
 <body>
-	<div class="site-page">
-		<div class="site-header">
-			<a href="/">
-				<img src="<?php bloginfo( "template_url" ); ?>/images/header-me.png" class="logo" />
-			</a>
+    <div class="site-page">
+        <div class="site-header">
+            <a href="/">
+                <img src="<?php bloginfo( "template_url" ); ?>/images/header-me.png" class="site-logo" />
+            </a>
 
-			<div class="header-detail">
-				<h1 class="site-title shortened-name">P Keating</h1>
-				<h1 class="site-title full-name">Peter Keating</h1>
+            <h1 class="site-title site-title-short">P Keating</h1>
+            <h1 class="site-title site-title-full">Peter Keating</h1>
 
-				<ul class="nav site-nav">
-					<li><a href="/">Home</a></li>
-					<li><a href="/about" class="site-nav-item">About</a></li>
-					<li><a href="/archive" class="site-nav-item">Archive</a></li>
-				</ul>
+            <ul class="nav site-nav">
+                <li><a href="/">Home</a></li>
+                <li><a href="/about" class="site-nav-item">About</a></li>
+                <li><a href="/archive" class="site-nav-item">Archive</a></li>
+            </ul>
 
-				<ul class="nav social-nav">
-					<li>
-						<a href="http://twitter.com/peterkeating" title="View my Twitter profile." target="_blank">
-							<div class="social-media-icon icon-twitter"></div>
-						</a>
-					</li>
+            <ul class="nav social-nav">
+                <li>
+                    <a href="http://twitter.com/peterkeating" title="View my Twitter profile." target="_blank">
+                        <div class="social-media-icon icon-twitter"></div>
+                    </a>
+                </li>
 
-					<li>
-						<a href="http://github.com/peterkeating" title="View my Github profile." target="_blank">
-							<div class="social-media-icon icon-github"></div>
-						</a>
-					</li>
+                <li>
+                    <a href="http://github.com/peterkeating" title="View my Github profile." target="_blank">
+                        <div class="social-media-icon icon-github"></div>
+                    </a>
+                </li>
 
-					<li>
-						<a href="http://www.endomondo.com/profile/5042109" title="View my Endomondo profile." target="_blank">
-							<div class="social-media-icon icon-endomondo"></div>
-						</a>
-					</li>
-				</ul>
-			</div>
-		</div>
+                <li>
+                    <a href="http://www.endomondo.com/profile/5042109" title="View my Endomondo profile." target="_blank">
+                        <div class="social-media-icon icon-endomondo"></div>
+                    </a>
+                </li>
+            </ul>
+        </div>
 
-		<div class="main">
+        <div class="main">
 
 
