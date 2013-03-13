8158cd3f782556354e146b7cba5955a656394165
Peter Keating <peter@moov2.com>
2013-03-13 22:46:15 +0000

    moved vars to the top of the function as per jslint suggestions.

--- js/webp.js
+++ js/webp.js
@@ -1,19 +1,21 @@
 window.onload  = function () {
+    var i, images, imageType;
+
     /**
      * Gets all the <img> tags in the DOM.
      */
-    var images = document.getElementsByTagName('img');
+    images = document.getElementsByTagName('img');
 
     /**
      * Image type is determined based on webp being supported.
      */
-    var imageType = (Modernizr.webp) ? '.webp' : '.png';
+    imageType = (Modernizr.webp) ? '.webp' : '.png';
 
     /**
      * Loops through the images loaded via the <img> tag changing the src to load
      * a .png image that is supported by all browsers.
      */
-    for(var i = 0; i < images.length; i++) {
+    for(i = 0; i < images.length; i++) {
         if (images[i].hasAttribute('data-src')) {
             images[i].src = images[i].getAttribute('data-src') + imageType;
         }
