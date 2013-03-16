d29b7d52938a071a8de7090ebd92110ec1cfc0f7
Peter Keating <peter@moov2.com>
2013-03-16 10:54:03 +0000

    removed unused class from site logo.

--- header.php
+++ header.php
@@ -30,10 +30,10 @@
         <div class="site-header">
             <a href="/">
                 <!--[if gt IE 8]><!-->
-                <img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="<?php bloginfo( "template_url" ); ?>/images/me" class="modern-logo site-logo" alt="Photo of Peter Keating." title="Photo of Peter Keating." />
+                <img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" data-src="<?php bloginfo( "template_url" ); ?>/images/me" class="site-logo" alt="Photo of Peter Keating." title="Photo of Peter Keating." />
 
                 <noscript>
-                    <img src="<?php bloginfo( "template_url" ); ?>/images/me.png" class="modern-logo site-logo" alt="Photo of Peter Keating." title="Photo of Peter Keating." />
+                    <img src="<?php bloginfo( "template_url" ); ?>/images/me.png" class="site-logo" alt="Photo of Peter Keating." title="Photo of Peter Keating." />
                 </noscript>
                 <!--<![endif]-->
 
