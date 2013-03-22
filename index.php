f9f16dac2e1d961b5de49080699a4dbf44c03ddd
Peter Keating <peter@moov2.com>
2013-03-22 17:06:47 +0000

    post date & tags now only appear once in the markup.

--- index.php
+++ index.php
@@ -4,23 +4,16 @@
 <?php while ( have_posts () ) : the_post() ?>
 
 	<div class="post-summary">
-		<div class="wide-post-meta">
+		<h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+		<div class="post-meta">
 			<div class="post-publish-date date-icon"><?php the_time('F j, Y') ?></div>
 			<div class="post-tags tag-icon"><?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?></div>
 		</div>
 
-		<div class="the-post">
-			<h2 class="post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+		<div class="post-snippet"><?php the_excerpt(); ?></div>
 
-			<div class="post-meta">
-				<div class="post-publish-date date-icon"><?php the_time('F j, Y') ?></div>
-				<div class="post-tags tag-icon"><?php the_tags('<ul><li>','</li><li>','</li></ul>'); ?></div>
-			</div>
-
-			<div class="post-snippet"><?php the_excerpt(); ?></div>
-
-			<a href="<?php the_permalink(); ?>" class="post-link">Continue Reading</a>
-		</div>
+		<a href="<?php the_permalink(); ?>" class="post-link">Continue Reading</a>
 	</div>
 
 <?php endwhile; ?>
